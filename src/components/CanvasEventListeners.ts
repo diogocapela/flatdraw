82791e4b08b646cae0c6fd912bfbb12f4fda48cd
Diogo Capela <diogocapela@gmail.com>
2023-08-09 19:59:07 +0100

    Close icon and image controls on canvas click

--- src/components/CanvasEventListeners.ts
+++ src/components/CanvasEventListeners.ts
@@ -1,17 +1,23 @@
 import { useEffect } from 'react';
 
-import { APP_FIXED_MAIN_UNIQUE_ID } from '~/config/globalElementIds';
+import { APP_FIXED_MAIN_UNIQUE_ID, CANVAS_CONTROLS_OVERLAY } from '~/config/globalElementIds';
+import { UserMode } from '~/config/types';
 import useCanvasContext from '~/context/canvas/useCanvasContext';
 import useModalContext from '~/context/useModalContext';
 import useActiveObjectId from '~/store/useActiveObjectId';
 import useCanvasObjects from '~/store/useCanvasObjects';
 import useScrollPosition from '~/store/useScrollPosition';
+import useUserMode from '~/store/useUserMode';
 import useWindowSize from '~/store/useWindowSize';
 
 export default function CanvasEventListeners() {
-  const { isModalActive } = useModalContext();
   const { canvasRef, initCanvas, drawEverything } = useCanvasContext();
+  const { isModalActive } = useModalContext();
+
   const activeObjectId = useActiveObjectId((state) => state.activeObjectId);
+
+  const userMode = useUserMode((state) => state.userMode);
+  const setUserMode = useUserMode((state) => state.setUserMode);
 
   const setWindowSize = useWindowSize((state) => state.setWindowSize);
 
@@ -33,6 +39,7 @@ export default function CanvasEventListeners() {
       initCanvas();
       drawEverything();
     };
+
     window.addEventListener('resize', onResize);
     return () => {
       window.removeEventListener('resize', onResize);
@@ -56,18 +63,36 @@ export default function CanvasEventListeners() {
     };
   }, [canvasRef, isModalActive, updateScrollPosition]);
 
+  // Pointerdown event
+
+  useEffect(() => {
+    const onPointerDown = (event: PointerEvent) => {
+      if (isModalActive) return;
+      const wasCanvasClick = (event.target as HTMLElement)?.id === CANVAS_CONTROLS_OVERLAY;
+      const userModesToClose: UserMode[] = ['icon', 'image'];
+      if (wasCanvasClick && userModesToClose.includes(userMode)) {
+        setUserMode('select');
+      }
+    };
+
+    window.addEventListener('pointerdown', onPointerDown);
+    return () => {
+      window.removeEventListener('pointerdown', onPointerDown);
+    };
+  }, [isModalActive, setUserMode, userMode]);
+
   // Keydown event
 
   useEffect(() => {
     const onKeyDown = (event: KeyboardEvent) => {
       if (isModalActive) return;
-
       const isInputFocused = ['input', 'textarea'].includes(document.activeElement?.localName || '');
-
-      if (['Backspace', 'Delete'].includes(event.key) && !isInputFocused && activeObjectId) {
+      const deleteKeys: KeyboardEvent['key'][] = ['Backspace', 'Delete'];
+      if (deleteKeys.includes(event.key) && !isInputFocused && activeObjectId) {
         deleteCanvasObject(activeObjectId);
       }
     };
+
     window.addEventListener('keydown', onKeyDown);
     return () => {
       window.removeEventListener('keydown', onKeyDown);
